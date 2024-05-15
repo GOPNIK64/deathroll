4de5b87c77ff30d7578b49f43149eb6db226fd6c
GOPNIK <161072764+GOPNIK64@users.noreply.github.com>
2024-05-15 12:56:20 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,210 @@
+const numeroIngresado = [];
+
+const displayTexto = document.querySelector(".displayNumero")
+
+const displayTextoApuesta = document.querySelector(".displayApuesta")
+
+// CONSTANTES DEL JUEGO
+const unoDeathroll = 1
+
+const botonDeRoll = document.querySelector("#roll")
+
+let numeroApuesta = ""
+
+// display.document.
+
+document.addEventListener('click', (e) => {
+
+    if(e.target.id === "roll"){
+
+        const numeroRoleado = Math.floor(Math.random() * (numeroApuesta - unoDeathroll) + unoDeathroll);
+                   
+            displayTexto.textContent = numeroRoleado
+    
+    
+            if(numeroRoleado > 1){
+    
+                numeroApuesta = numeroRoleado
+            
+            
+            }
+            if(numeroRoleado === 1) {
+                
+                displayTexto.textContent = "PERDISTE"
+            
+            }
+
+            
+    }
+
+    // const display = +(numeroIngresado.join(''));
+
+    if(e.target.id === "0"){
+        // console.log(0)
+        numeroIngresado.push("0")
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+    };
+    if(e.target.id === "1"){
+        // console.log(1)
+        numeroIngresado.push("1");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+        
+        numeroApuesta = display
+    
+    };
+    if(e.target.id === "2"){
+        // console.log("2")
+        numeroIngresado.push("2");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+    };
+    if(e.target.id === "3"){
+        // console.log("3")
+        numeroIngresado.push("3");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+    };
+    if(e.target.id === "4"){
+        // console.log("4")
+        numeroIngresado.push("4");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+    };
+    if(e.target.id === "5"){
+        // console.log("5")
+        numeroIngresado.push("5");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+    };
+    if(e.target.id === "6"){
+        // console.log("6")
+        numeroIngresado.push("6");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+    };
+    if(e.target.id === "7"){
+        // console.log("7")
+        numeroIngresado.push("7");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+    };
+    if(e.target.id === "8"){
+        // console.log("8")
+        numeroIngresado.push("8");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+    };
+    if(e.target.id === "9"){
+        // console.log("9")
+        numeroIngresado.push("9");
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+  
+    };
+
+    if(e.target.id === "X"){
+        numeroIngresado.length = 0
+
+        console.log(numeroIngresado)
+
+        const display = +(numeroIngresado.join(''));
+
+        console.log(display)
+
+        displayTexto.textContent = display
+
+        numeroApuesta = display
+
+
+    };
+});
+
